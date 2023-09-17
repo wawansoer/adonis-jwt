1569c4fb745132fbcb67c168d72ed11e08b286a6
Sutisna Utamasjida Septiawan <wawansoer@gmail.com>
2023-09-17 18:28:46 +0700

    init

---
+++ database/migrations/1694912437381_users.ts
@@ -0,0 +1,26 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary().index()
+      table.string('email', 255).notNullable().unique().index()
+      table.string('username', 100).notNullable().unique().index()
+      table.boolean('is_active').notNullable().defaultTo(false)
+      table.string('password', 180).notNullable()
+      table.string('remember_me_token').nullable()
+
+      /**
+       * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).notNullable()
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
---
+++ database/migrations/1694932419559_tokens.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'tokens'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.uuid('user_id').references('id').inTable('users').onDelete('CASCADE')
+      table.string('token', 64).notNullable().unique().index()
+      table.timestamp('expires_at', { useTz: true }).notNullable()
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
