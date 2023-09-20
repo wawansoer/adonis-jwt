eb696362093d4bb6d578594c2a2eea7af4df134d
Sutisna Utamasjida Septiawan <wawansoer@gmail.com>
2023-09-21 06:21:27 +0700

    fix : Auth Controller

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -2,14 +2,12 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { DateTime } from 'luxon'
 import Mail from '@ioc:Adonis/Addons/Mail'
 import Logger from '@ioc:Adonis/Core/Logger'
-import { v4 as uuid } from 'uuid'
 import Database from '@ioc:Adonis/Lucid/Database'
 import Env from '@ioc:Adonis/Core/Env'
 import { generateRandomString } from '../../Helpers/GenerateRandomString'
 import User from '../../Models/User'
 import Token from '../../Models/Token'
 import RegisterValidator from '../../Validators/Auth/RegisterValidator'
-import ForgotPasswordValidator from '../../Validators/Auth/ForgotPasswordValidator'
 import ApiToken from '../../Models/ApiToken'
 
 export default class AuthController {
@@ -18,7 +16,7 @@ export default class AuthController {
 			const token = new ApiToken()
 			token.userId = userId
 			token.name = 'Email Confirmation'
-			// token.type = 'UUID'
+			token.type = 'UUID'
 			token.token = generateRandomString(64)
 			token.expiresAt = DateTime.now().plus({ hours: 1 })
 
@@ -92,112 +90,110 @@ export default class AuthController {
 				.where('expires_at', '>=', DateTime.now().toString())
 				.firstOrFail()
 
-			if (tokenRecord) {
-				const user = await User.findBy('email', email)
+			const user = await User.findBy('email', email)
 
-				if (user) {
-					user.is_verified = true
-					await user.save()
-					return response.status(200).json({
-						success: true,
-						message: 'Account activated successfully',
-					})
-				}
+			if (user) {
+				user.is_verified = true
+				await user.save()
+
+				return response.status(200).json({
+					success: true,
+					message: 'Account activated successfully',
+				})
 			}
 
 			return response.status(404).json({
 				success: false,
-				message: 'Failed to activate the account',
-				data: null,
+				message: 'User or token not found',
 			})
 		} catch (error) {
-			// Handle any errors here, log them, and respond accordingly
 			Logger.error(error)
-			return response.status(404).json({
+
+			return response.status(500).json({
 				success: false,
 				message: 'Failed to activate the account',
-				data: null,
+				error : error
 			})
 		}
 	}
 
-	public async resendToken({ request, response }: HttpContextContract) {
-		const params = request.only(['email'])
-
-		const user = await User.findBy('email', params.email)
-
-		if (user) {
-			await this.sendToken(user.id, user.email, user.username)
-			return response.status(404).json({
-				success: true,
-				message: `Successfully sent token to ${user.email}`,
-			})
-		}
-
-		return response.status(404).json({
-			success: false,
-			message: 'User not found',
-			data: null,
-		})
-	}
-
-	public async reqTokenResetPassword({ request, response }: HttpContextContract) {
-		const params = request.only(['email'])
-
-		const user = await User.findBy('email', params.email)
-
-		if (user) {
-			const generate_token = uuid()
-			const token = new Token()
-			token.userId = user.id
-			token.token = generate_token
-			token.expiresAt = DateTime.now().plus({ hours: 1 })
-			let base_url = Env.get('FRONT_END_URL')
-
-			await Mail.send((message) => {
-				message
-					.from(`${Env.get('SMTP_USERNAME')}`)
-					.to(user.email)
-					.subject('Reset Password Request')
-					.html(
-						`<h3> Hi, ${user.username}</h3>
-						<p>
-							Please click this
-							<a href='${base_url}/reset-password/${user.id}/token/${generate_token}'>Link</a>
-							to reset your password
-						</p>`
-					)
-			})
-
-			return response.status(404).json({
-				success: true,
-				message: `Successfully sent token to ${user.email}`,
-			})
-		}
-
-		return response.status(404).json({
-			success: false,
-			message: 'User not found',
-			data: null,
-		})
-	}
-	public async updatePassword({ request, response }: HttpContextContract) {
-		let data = await request.validate(ForgotPasswordValidator)
-
-		const user = await User.findBy('email', data.email)
-
-		if (!user) {
-			return response.status(404).json({
-				success: false,
-				message: 'User not found',
-			})
-		}
-
-		return response.status(200).json({
-			success: true,
-			message: `Verification token have sent to ${user.email} !`,
-		})
-	}
+	// public async resendToken({ request, response }: HttpContextContract) {
+	// 	const params = request.only(['email'])
+	//
+	// 	const user = await User.findBy('email', params.email)
+	//
+	// 	if (user) {
+	// 		await this.sendToken(user.id, user.email, user.username)
+	// 		return response.status(404).json({
+	// 			success: true,
+	// 			message: `Successfully sent token to ${user.email}`,
+	// 		})
+	// 	}
+	//
+	// 	return response.status(404).json({
+	// 		success: false,
+	// 		message: 'User not found',
+	// 		data: null,
+	// 	})
+	// }
+	//
+	// public async reqTokenResetPassword({ request, response }: HttpContextContract) {
+	// 	const params = request.only(['email'])
+	//
+	// 	const user = await User.findBy('email', params.email)
+	//
+	// 	if (user) {
+	// 		const generate_token = uuid()
+	// 		const token = new Token()
+	// 		token.userId = user.id
+	// 		token.token = generate_token
+	// 		token.expiresAt = DateTime.now().plus({ hours: 1 })
+	// 		let base_url = Env.get('FRONT_END_URL')
+	//
+	// 		await Mail.send((message) => {
+	// 			message
+	// 				.from(`${Env.get('SMTP_USERNAME')}`)
+	// 				.to(user.email)
+	// 				.subject('Reset Password Request')
+	// 				.html(
+	// 					`<h3> Hi, ${user.username}</h3>
+	// 					<p>
+	// 						Please click this
+	// 						<a href='${base_url}/reset-password/${user.id}/token/${generate_token}'>Link</a>
+	// 						to reset your password
+	// 					</p>`
+	// 				)
+	// 		})
+	//
+	// 		return response.status(404).json({
+	// 			success: true,
+	// 			message: `Successfully sent token to ${user.email}`,
+	// 		})
+	// 	}
+	//
+	// 	return response.status(404).json({
+	// 		success: false,
+	// 		message: 'User not found',
+	// 		data: null,
+	// 	})
+	// }
+	// public async updatePassword({ request, response }: HttpContextContract) {
+	// 	let data = await request.validate(ForgotPasswordValidator)
+	//
+	// 	const user = await User.findBy('email', data.email)
+	//
+	// 	if (!user) {
+	// 		return response.status(404).json({
+	// 			success: false,
+	// 			message: 'User not found',
+	// 		})
+	// 	}
+	//
+	// 	return response.status(200).json({
+	// 		success: true,
+	// 		message: `Verification token have sent to ${user.email} !`,
+	// 	})
+	// }
 
 	public async login({ request, auth }: HttpContextContract) {
 		const data = request.only(['email', 'password'])
