7632784b8e20c91e6530e29702d7193d22a2d378
Sutisna Utamasjida Septiawan <wawansoer@gmail.com>
2023-09-22 04:21:45 +0700

    fix : enhance code readability

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -4,41 +4,48 @@ import Mail from '@ioc:Adonis/Addons/Mail'
 import Logger from '@ioc:Adonis/Core/Logger'
 import Database from '@ioc:Adonis/Lucid/Database'
 import Env from '@ioc:Adonis/Core/Env'
-import { generateRandomString } from '../../Helpers/GenerateRandomString'
+import { v4 as uuid } from 'uuid'
 import User from '../../Models/User'
 import ApiToken from '../../Models/ApiToken'
 import RegisterValidator from '../../Validators/Auth/RegisterValidator'
 
+const ACTION_ACCOUNT_VERIFICATION = 'Account Verification'
+
 export default class AuthController {
-	private async generateToken(userId: string, action: string, trx) {
-		try {
-			const token = new ApiToken()
-			token.userId = userId
-			token.name = action
-			token.type = 'TOKEN 64'
-			token.token = generateRandomString(64)
-			token.expiresAt = DateTime.now().plus({ hours: 1 })
-
-			await token.useTransaction(trx).save()
-
-			return token
-		} catch (e) {
-			Logger.error(e)
-			throw new Error('Failed to generate token')
-		}
+	/**
+	 * Create a new user and save it to the database.
+	 */
+	private async createUser(email: string, password: string, username: string, trx): Promise<User> {
+		const user = new User()
+		user.email = email
+		user.password = password
+		user.username = username
+		return await user.useTransaction(trx).save()
 	}
 
-	private async sendEmail(user: User, token: ApiToken, action: string) {
-		const base_url = Env.get('FRONT_END_URL')
-		let url, msg
+	/**
+	 * Generate a verification token for the given user and save it to the database.
+	 */
+	private async generateVerificationToken(userId: string, trx): Promise<ApiToken> {
+		const token = new ApiToken()
+		token.userId = userId
+		token.name = ACTION_ACCOUNT_VERIFICATION
+		token.type = 'UUID'
+		token.token = uuid()
+		token.expiresAt = DateTime.now().plus({ hours: 1 })
 
-		if (action === 'Account Verification') {
-			url = `${base_url}/verify-email?token=${token.token}&$email=${user.email}`
-			msg = `Tap the button below to confirm your email address. If you didn't create an account, you can safely delete this email.`
-		} else if (action === 'Reset Password') {
-			url = `${base_url}/verify-email?token=${token.token}&$email=${user.email}`
-			msg = `Tap the button below to reset your password. If you didn't request reset password, you can safely delete this email.`
-		}
+		await token.useTransaction(trx).save()
+
+		return token
+	}
+
+	/**
+	 * Send a verification email to the given user with the verification token.
+	 */
+	private async sendVerificationEmail(user: User, token: ApiToken) {
+		const base_url = Env.get('FRONT_END_URL')
+		const url = `${base_url}/verify-email?token=${token.token}&$email=${user.email}`
+		const msg = `Tap the button below to confirm your email address. If you didn't create an account, you can safely delete this email.`
 
 		await Mail.send((message) => {
 			message
@@ -48,28 +55,26 @@ export default class AuthController {
 				.htmlView('emails/welcome.edge', {
 					username: user.username,
 					url: url,
-					type_of_action: action,
+					type_of_action: ACTION_ACCOUNT_VERIFICATION,
 					message: msg,
 					from: Env.get('SMTP_USERNAME'),
 				})
 		})
 	}
 
+	/**
+	 * Register a new user.
+	 */
 	public async register({ request, response }: HttpContextContract) {
 		const trx = await Database.transaction()
 		try {
 			const data = await request.validate(RegisterValidator)
 
-			const user = new User()
-			user.email = data.email
-			user.password = data.password
-			user.username = data.username
+			const user = await this.createUser(data.email, data.password, data.username, trx)
 
-			await user.useTransaction(trx).save()
+			const token = await this.generateVerificationToken(user.id, trx)
 
-			const token = await this.generateToken(user.id, 'Account Verification', trx)
-
-			await this.sendEmail(user, token, 'Account Verification')
+			await this.sendVerificationEmail(user, token)
 
 			await trx.commit()
 
@@ -80,25 +85,35 @@ export default class AuthController {
 			})
 		} catch (error) {
 			Logger.error(error)
+			if (trx && trx.isTransaction) {
+				await trx.rollback()
+			}
 
-			await trx.rollback()
+			let errorMessage = error.messages
+				? 'Validation failed'
+				: 'Failed to send email confirmation'
 
 			return response.status(error.messages ? 400 : 500).json({
 				success: false,
-				message: error.messages ? 'Validation failed' : 'Failed to send email conformation',
+				message: errorMessage,
 				error: error.messages ? error.messages : error.message,
 			})
 		}
 	}
 
+	/**
+	 * Verify the email of a user using the verification token.
+	 */
 	public async verifyEmail({ request, response }: HttpContextContract) {
 		try {
 			const { token, email } = request.only(['token', 'email'])
 
-			const tokenRecord = await ApiToken.query()
+			const apiToken = await ApiToken.query()
 				.where('token', token)
 				.where('expires_at', '>=', DateTime.now().toString())
 				.firstOrFail()
+
+			await apiToken.delete() // Delete the verification token from the database
 
 			const user = await User.findBy('email', email)
 
@@ -110,12 +125,12 @@ export default class AuthController {
 					success: true,
 					message: 'Account activated successfully',
 				})
+			} else {
+				return response.status(404).json({
+					success: false,
+					message: 'User not found',
+				})
 			}
-
-			return response.status(404).json({
-				success: false,
-				message: 'User not found or token expired',
-			})
 		} catch (error) {
 			Logger.error(error)
 
@@ -127,90 +142,44 @@ export default class AuthController {
 		}
 	}
 
+	/**
+	 * Resend the verification token to the user's email.
+	 */
 	public async resendToken({ request, response }: HttpContextContract) {
 		const trx = await Database.transaction()
+		try {
+			const params = request.only(['email'])
 
-		const params = request.only(['email'])
+			if (params.email) {
+				const user = await User.query()
+					.where('email', params.email)
+					.where('is_verified', 0)
+					.firstOrFail()
 
-		if (params.email) {
-			const user = await User.findBy('email', params.email)
-
-			if (user) {
-				const token = await this.generateToken(user.id, 'Account Verification', trx)
-				await this.sendEmail(user, token, 'Account Verification')
+				const token = await this.generateVerificationToken(user.id, trx)
+				await this.sendVerificationEmail(user, token)
 				await trx.commit()
 				return response.status(200).json({
 					success: true,
 					message: `Successfully sent token to ${user.email}`,
 				})
 			}
-		}
+		} catch (error) {
+			if (trx && trx.isTransaction) {
+				await trx.rollback()
+			}
 
-		return response.status(400).json({
-			success: false,
-			message: 'User not found',
-		})
+			return response.status(500).json({
+				success: false,
+				message: 'Your account is not registered or has active',
+				error: error.message,
+			})
+		}
 	}
 
-	//
-	// public async reqTokenResetPassword({ request, response }: HttpContextContract) {
-	// 	const params = request.only(['email'])
-	//
-	// 	const user = await User.findBy('email', params.email)
-	//
-	// 	if (user) {
-	// 		const generate_token = uuid()
-	// 		const token = new Token()
-	// 		token.userId = user.id
-	// 		token.token = generate_token
-	// 		token.expiresAt = DateTime.now().plus({ hours: 1 })
-	// 		let base_url = Env.get('FRONT_END_URL')
-	//
-	// 		await Mail.send((message) => {
-	// 			message
-	// 				.from(`${Env.get('SMTP_USERNAME')}`)
-	// 				.to(user.email)
-	// 				.subject('Reset Password Request')
-	// 				.html(
-	// 					`<h3> Hi, ${user.username}</h3>
-	// 					<p>
-	// 						Please click this
-	// 						<a href='${base_url}/reset-password/${user.id}/token/${generate_token}'>Link</a>
-	// 						to reset your password
-	// 					</p>`
-	// 				)
-	// 		})
-	//
-	// 		return response.status(404).json({
-	// 			success: true,
-	// 			message: `Successfully sent token to ${user.email}`,
-	// 		})
-	// 	}
-	//
-	// 	return response.status(404).json({
-	// 		success: false,
-	// 		message: 'User not found',
-	// 		data: null,
-	// 	})
-	// }
-	// public async updatePassword({ request, response }: HttpContextContract) {
-	// 	let data = await request.validate(ForgotPasswordValidator)
-	//
-	// 	const user = await User.findBy('email', data.email)
-	//
-	// 	if (!user) {
-	// 		return response.status(404).json({
-	// 			success: false,
-	// 			message: 'User not found',
-	// 		})
-	// 	}
-	//
-	// 	return response.status(200).json({
-	// 		success: true,
-	// 		message: `Verification token have sent to ${user.email} !`,
-	// 	})
-	// }
-
+	/**
+	 * Login a user and generate a JWT token.
+	 */
 	public async login({ request, auth }: HttpContextContract) {
 		const data = request.only(['email', 'password'])
 		const user = await User.findBy('email', data.email)
