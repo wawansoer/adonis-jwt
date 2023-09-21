3370ec4804e47b794e83be7368d121b80eea2928
Sutisna Utamasjida Septiawan <wawansoer@gmail.com>
2023-09-22 04:22:00 +0700

    feat : Role Migration

---
+++ database/migrations/1695256594269_roles.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+	protected tableName = 'roles'
+
+	public async up() {
+		this.schema.createTable(this.tableName, (table) => {
+			table.uuid('id').primary().unique()
+			table.string('slug').notNullable().unique()
+			table.string('name').notNullable().unique()
+			table.text('description').nullable()
+
+			/**
+			 * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+			 */
+			table.timestamp('created_at', { useTz: true })
+			table.timestamp('updated_at', { useTz: true })
+		})
+	}
+
+	public async down() {
+		this.schema.dropTable(this.tableName)
+	}
+}
