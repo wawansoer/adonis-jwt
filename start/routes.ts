bc1a2891aaed530d2c58e3f2580c3bc957a90bc3
Sutisna Utamasjida Septiawan <wawansoer@gmail.com>
2023-09-22 08:24:17 +0700

    fix : auth route

--- start/routes.ts
+++ start/routes.ts
@@ -21,12 +21,14 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async () => {
-	return { hello: 'world' }
-}).middleware('auth:jwt')
+	return { message: 'This is Adonis 5 Service' }
+})
 
-Route.post('/register', 'AuthController.register')
-Route.get('/verify-email', 'AuthController.verifyEmail')
-Route.post('/resend-token', 'AuthController.resendToken')
-Route.post('/forgot-password', 'AuthController.reqTokenResetPassword')
-Route.post('/verify-token', 'AuthController.verifyToken')
-Route.post('/login', 'AuthController.login')
+Route.group(() => {
+	Route.post('/register', 'AuthController.register')
+	Route.get('/verify-email', 'AuthController.verifyEmail')
+	Route.post('/resend-token', 'AuthController.resendToken')
+	Route.post('/verify-token', 'AuthController.verifyToken')
+	Route.post('/login', 'AuthController.login')
+	Route.post('/forgot-password', 'AuthController.forgotPassword')
+}).prefix('api/v1/auth')
