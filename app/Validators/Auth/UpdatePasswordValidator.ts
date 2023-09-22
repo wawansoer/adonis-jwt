1e95b74c7c5a6bdf22ad014c77a7f27816ebe105
Sutisna Utamasjida Septiawan <wawansoer@gmail.com>
2023-09-23 03:20:22 +0700

    feat : update password validator

---
+++ app/Validators/Auth/UpdatePasswordValidator.ts
@@ -0,0 +1,50 @@
+import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class UpdatePasswordValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  /*
+   * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
+   *
+   * For example:
+   * 1. The username must be of data type string. But then also, it should
+   *    not contain special characters or numbers.
+   *    ```
+   *     schema.string({}, [ rules.alpha() ])
+   *    ```
+   *
+   * 2. The email must be of data type string, formatted as a valid
+   *    email. But also, not used by any other user.
+   *    ```
+   *     schema.string({}, [
+   *       rules.email(),
+   *       rules.unique({ table: 'users', column: 'email' }),
+   *     ])
+   *    ```
+   */
+  public schema = schema.create({
+	  email: schema.string({ trim: true }, [rules.email()]),
+	  password: schema.string({trim: true}, [rules.minLength(8)]),
+	  token: schema.string({ trim: true }, [rules.uuid({ version: 4 })]),
+  })
+
+  /**
+   * Custom messages for validation failures. You can make use of dot notation `(.)`
+   * for targeting nested fields and array expressions `(*)` for targeting all
+   * children of an array. For example:
+   *
+   * {
+   *   'profile.username.required': 'Username is required',
+   *   'scores.*.number': 'Define scores as valid numbers'
+   * }
+   *
+   */
+  public messages: CustomMessages = {
+	  'required': '{{ field }} is required',
+	  'email.email': 'Invalid email address format',
+	  'minLength': '{{ field }} must be at least {{ options.minLength }} characters long',
+	  'maxLength': '{{ field }} must be less then {{ options.maxLength }} characters long',
+	  'token.uuid' : 'Invalid token format'
+  }
+}
