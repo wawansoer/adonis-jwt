a29806737f82c275a74951208c68d9058bfae006
Sutisna Utamasjida Septiawan <wawansoer@gmail.com>
2023-09-25 22:12:35 +0700

    fix : build

---
+++ build/database/migrations/1695256594269_roles.js
@@ -0,0 +1,27 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const Schema_1 = __importDefault(global[Symbol.for('ioc.use')]("Adonis/Lucid/Schema"));
+class default_1 extends Schema_1.default {
+    constructor() {
+        super(...arguments);
+        this.tableName = 'roles';
+    }
+    async up() {
+        this.schema.createTable(this.tableName, (table) => {
+            table.uuid('id').primary().unique();
+            table.string('slug').notNullable().unique();
+            table.string('name').notNullable().unique();
+            table.text('description').nullable();
+            table.timestamp('created_at', { useTz: true });
+            table.timestamp('updated_at', { useTz: true });
+        });
+    }
+    async down() {
+        this.schema.dropTable(this.tableName);
+    }
+}
+exports.default = default_1;
+//# sourceMappingURL=1695256594269_roles.js.map
