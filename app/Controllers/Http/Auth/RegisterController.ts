5abf725345b73ae05db123abfb344fd8791d662c
Sutisna Utamasjida Septiawan <wawansoer@gmail.com>
2023-10-27 23:14:18 +0700

    feat : add chain user detail table after register

--- app/Controllers/Http/Auth/RegisterController.ts
+++ app/Controllers/Http/Auth/RegisterController.ts
@@ -1,9 +1,9 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Logger from '@ioc:Adonis/Core/Logger'
 import Database from '@ioc:Adonis/Lucid/Database'
 import AuthService from '../../../Services/AuthService'
 import RegisterValidator from '../../../Validators/Auth/RegisterValidator'
 import Role from '../../../Models/Role'
-import Logger from '@ioc:Adonis/Core/Logger'
 import { Response } from '../../../Interfaces/Response'
 
 export default class RegisterController {
@@ -28,6 +28,7 @@ export default class RegisterController {
 				trx
 			)
 			await this.authService.createUserRole(user.id, role.id, trx)
+			await this.authService.initUserDetail(user.id, trx)
 			const token = await this.authService.generateToken(user.id, 'Account Verification', trx)
 			await this.authService.sendEmail(user, token, 'Account Verification')
 			await trx.commit()
@@ -38,14 +39,14 @@ export default class RegisterController {
 				await trx.rollback()
 			}
 			let errorMessage = error.messages
-				? 'Validation failed'
-				: 'Failed to send email confirmation'
+				? 'Validation failed.'
+				: 'Failed to send email confirmation make sure you have a valid email'
 			Response(
 				response,
 				error.messages ? 400 : 500,
 				false,
 				errorMessage,
-				error.messages.errors ?? '',
+				error.messages ? error.messages : '',
 				error
 			)
 		}
