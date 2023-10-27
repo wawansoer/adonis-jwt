b184c174af3f0380190cc236e57f1b16c1225b63
Sutisna Utamasjida Septiawan <wawansoer@gmail.com>
2023-10-27 23:12:30 +0700

    feat : add user detail controller

--- app/Controllers/Http/UserDetailsController.ts
+++ app/Controllers/Http/UserDetailsController.ts
@@ -1,7 +1,7 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import UserDetail from '../../Models/UserDetail'
-import CreateValidator from '../../Validators/UserDetail/CreateValidator'
 import UpdateValidator from '../../Validators/UserDetail/UpdateValidator'
+import { Response } from '../../Interfaces/Response'
 
 export default class UserDetailsController {
 	public async index({ response }: HttpContextContract) {
@@ -12,76 +12,22 @@ export default class UserDetailsController {
 	public async show({ params, response }: HttpContextContract) {
 		try {
 			const userDetail = await UserDetail.query().where('user_id', params.id).firstOrFail()
-			return response.status(201).json({
-				success: true,
-				message: 'Successfully get data',
-				data: userDetail,
-			})
+			Response(response, 200, true, 'Successfully get data', userDetail)
 		} catch (e) {
-			return response.status(404).json({
-				success: false,
-				message: 'Failed get user data',
-				error: e,
-			})
-		}
-	}
-
-	public async store({ request, response }: HttpContextContract) {
-		try {
-			const data = await request.validate(CreateValidator)
-			const userDetail = await UserDetail.create(data)
-			return response.status(201).json({
-				success: true,
-				message: 'Successfully saved data',
-				data: userDetail,
-			})
-		} catch (e) {
-			return response.status(404).json({
-				success: false,
-				message: 'Failed saved user data',
-				error: e,
-			})
+			Response(response, 404, false, 'Failed get data', '', e)
 		}
 	}
 
 	public async update({ params, request, response }: HttpContextContract) {
 		try {
-			const userDetail = await UserDetail.findOrFail(params.id)
-
+			const userDetail = await UserDetail.query().where('user_id', params.id).firstOrFail()
 			const data = await request.validate(UpdateValidator)
-
 			userDetail.merge(data)
 			await userDetail.save()
 
-			return response.status(200).json({
-				success: true,
-				message: 'Successfully updated data',
-				data: userDetail,
-			})
+			Response(response, 200, true, 'Successfully update data', userDetail)
 		} catch (e) {
-			return response.status(400).json({
-				success: false,
-				message: 'Failed update user data',
-				error: e,
-			})
-		}
-	}
-
-	public async destroy({ params, response }: HttpContextContract) {
-		try {
-			const userDetail = await UserDetail.findOrFail(params.id)
-			await userDetail.delete()
-			return response.status(200).json({
-				success: true,
-				message: 'Successfully deleted data',
-				data: userDetail,
-			})
-		} catch (e) {
-			return response.status(400).json({
-				success: false,
-				message: 'failed deleted data',
-				error: e,
-			})
+			Response(response, 400, false, 'Failed update data', '', e)
 		}
 	}
 }
