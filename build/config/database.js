d81b4a54963a2fed1694f96e3c5f7fd0fd9023f4
Sutisna Utamasjida Septiawan <sutisna.utamasjida@govassist.com>
2024-01-14 13:12:08 +0700

    build

--- build/config/database.js
+++ build/config/database.js
@@ -7,6 +7,21 @@ const Env_1 = __importDefault(global[Symbol.for('ioc.use')]("Adonis/Core/Env"));
 const databaseConfig = {
     connection: Env_1.default.get('DB_CONNECTION'),
     connections: {
+        mysql: {
+            client: 'mysql2',
+            connection: {
+                host: Env_1.default.get('MYSQL_HOST'),
+                port: Env_1.default.get('MYSQL_PORT'),
+                user: Env_1.default.get('MYSQL_USER'),
+                password: Env_1.default.get('MYSQL_PASSWORD', ''),
+                database: Env_1.default.get('MYSQL_DB_NAME'),
+            },
+            migrations: {
+                naturalSort: true,
+            },
+            healthCheck: true,
+            debug: false,
+        },
         pg: {
             client: 'pg',
             connection: {
